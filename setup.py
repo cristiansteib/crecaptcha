f6c71d406639fd32179412c425c40ab7f36ce6b8
Cristian Steib <cristiansteib@gmail.com>
2018-10-16 21:19:13 -0300

    update classifiers

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='crecaptcha',
-    version='0.1.3',
+    version='0.1.32',
     author='Steib Cristian',
     author_email='cristiansteib@gmail.com',
     packages=['crecatpcha'],
@@ -17,6 +17,7 @@ setup(
         "Framework :: Django",
         "Framework :: Flask",
         "License :: OSI Approved :: Apache Software License",
-        "Topic :: Security"
+        "Topic :: Security",
+        "Development Status :: 5 - Production/Stable"
     ],
 )
